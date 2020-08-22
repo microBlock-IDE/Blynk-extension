35b403af8fd76dbc4a4cd9306af8c9da03922358
Max <max30012540@hotmail.com>
2020-08-22 12:29:54 +0700

    Update generators_blynk.js

--- blocks/generators_blynk.js
+++ blocks/generators_blynk.js
@@ -4,7 +4,6 @@ Blockly.Python['blynk_setup'] = function(block) {
   var value_auth = Blockly.Python.valueToCode(block, 'auth', Blockly.Python.ORDER_ATOMIC) || '""';
   var dropdown_debug = block.getFieldValue('debug');
 
-  Blockly.Python.addUploadModule("BlynkLib.py");
   Blockly.Python.definitions_['import_machine'] = 'import machine';
   Blockly.Python.definitions_['import_blynklib'] = 'import BlynkLib';
   Blockly.Python.definitions_['import_network'] = 'import network';
