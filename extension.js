5a579cb2a41cd486e4e76cbb5c3c3c0619fbc1bc
Max <max30012540@hotmail.com>
2020-08-23 17:36:02 +0700

    Update extension.js

--- extension.js
+++ extension.js
@@ -1,5 +1,9 @@
 ({
     name: "Blynk", // Category Name
+    description: "Blynk IoT platform: for businesses and developers",
+    author: "microBlock",
+    category: "Communication",
+    version: "1.0.0",
     icon: "/static/icon.png", // Category icon
     color: "#1ABC9C", // Category color (recommend some blocks color)
     blocks: [ // Blocks in Category
